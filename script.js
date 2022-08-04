55e32e913809693727f398bf0835bb72900bb499
jsLesson <jsLesson@gmail.com>
2022-08-04 13:44:07 +0300

    Add for

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let block = document.querySelector('.block');
 block.style.width = '200px';
 block.style.height = '200px';
 block.style.background = 'green';
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
